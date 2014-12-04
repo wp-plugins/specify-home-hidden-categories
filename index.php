ab9ece9a3f0b4311ad58534013d292c00e48d3bd
yzhs <yzhs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-04 07:11:03 +0000

    Plugin Frist Update 2014-12-04 By Zhys
    git-svn-id: https://plugins.svn.wordpress.org/specify-home-hidden-categories/trunk@1037977 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: Specify Home Hidden Categories
+Description: Specify the Homepage&RSS hidden under all the article
+Plugin URI: http://www.9sep.org/
+Version: 0.1.0
+Author: Zhys
+Author URI: http://www.9sep.org/author/zhys
+Author Email: Zhys@9sep.org
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Copyright 2014 九秒 (Zhys@9sep.org)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+defined( 'ABSPATH' ) OR exit;
+add_action( 'admin_init', 'specify_homepage_cats_init' );
+add_filter('pre_get_posts', 'specifycats');
+
+function specify_homepage_cats_init(){
+	add_settings_field(
+		'specify_cats',
+		'Specify Categories',
+		'specify_homepage_cats_callback_function',
+		'reading',
+		'default'
+	);
+
+	register_setting( 'reading', 'specify_cats' );
+}
+
+function specify_homepage_cats_callback_function(){
+    $options    = get_option('specify_cats');
+    $pag        = 'specify_cats';
+    $_cats      = get_terms( 'category' );
+    $html       = '';
+
+    foreach ($_cats as $term){
+    	if($options != ''){
+    		$checked = in_array($term->term_id, $options) ? 'checked="checked"' : '';
+    	}
+
+    	$html .= '<p class="sep9_l">';
+        $html .= sprintf( '<input type="checkbox" id="%1$s[%2$s]" name="%1$s[]" value="%2$s" %3$s />', $pag, $term->term_id, $checked );
+        $html .= sprintf( '<label for="%1$s[%3$s]"> %2$s</label><br>', $pag, $term->name, $term->term_id );
+        $html .= '</p>';
+    }
+
+    $html .= '<p class="sep"></p>';
+
+    echo $html;
+}
+
+function specifycats($query){
+if( is_home() ){
+	if ( $query->is_home ) {
+		$query->set('category__not_in', get_option('specify_cats'));
+	}	
+	if ( $query->is_feed ) {
+		$query->set('category__not_in', get_option('specify_cats'));
+	}	
+}
+	return $query;
+}
---
+++ readme.txt
@@ -0,0 +1,31 @@
+﻿=== Specify Home Hidden Categories ===
+Contributors: Zhys
+Tags: exclude, hide, related, categories, options, Homepage, Home, feed, rss, get_posts , have_posts, the_post
+Donate link: http://www.9sep.org/
+Stable tag: 0.1.0
+Requires at least: 3.9
+Tested up to: 4.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Specify the Home hidden under all the article，Set up one or more categories, as long as the article is contained in the specified category, then the article will be Home and in RSS were hidden, and can see only guest access to the category, it advantage is that do not affect other plugins with have_posts () and get_posts () ... Function access to all of the text. :)
+
+== Description ==
+English: Plugin help hide all posts in one or more category on Homepage. Go to \"Settings» Reading \"to choose the category need to hide
+Ps:No articles in the category are not of our choosing, so you should create a good classification part article and then make a selection
+
+中文:预先创建好一个或者多个用于隐藏的分类目录并确保分类中含有一篇文章。前往“设置”--->“阅读”，在最下方选择需要的隐藏分类吧，只有选定好需要隐藏的分类目录，分类目录下的所有文章不会在首页和RSS(首页的)上显示。
+
+== Installation ==
+1. Download zip file
+2. Upload the Simple Exclude Categories plugin to your blog
+3. Activate it
+
+== Screenshots ==
+1. Simple Exclude Categories Options Section
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+== Upgrade Notice ==
